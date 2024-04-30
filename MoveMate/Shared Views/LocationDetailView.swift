2590045b7bfb3e93bfacc8de54baff9e80105a54
Shiberger <hannarong.tonkra@gmail.com>
2024-05-01 02:33:04 +0700

    add func Marker Selection and LookaroundView

---
+++ MoveMate/Shared Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 1/5/2567 BE.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
