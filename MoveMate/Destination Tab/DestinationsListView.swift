1580344a10b6d5206678388d43f020741adf0585
Shiberger <hannarong.tonkra@gmail.com>
2024-05-01 00:38:34 +0700

    add DestinationListView + Navigation

---
+++ MoveMate/Destination Tab/DestinationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationsListView.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 1/5/2567 BE.
+//
+
+import SwiftUI
+
+struct DestinationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationsListView()
+}
