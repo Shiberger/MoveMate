5a8bc2bbf20ac900f0a433977ee03a018003abc3
Shiberger <hannarong.tonkra@gmail.com>
2024-04-30 17:31:33 +0700

    Initial Commit

---
+++ MoveMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 30/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoveMate/MoveMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMateApp.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 30/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MoveMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
