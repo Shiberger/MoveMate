ef8fde5deacac52f3f91d090acb4b9f332a927f5
Shiberger <hannarong.tonkra@gmail.com>
2024-05-01 00:19:39 +0700

    add SwiftData, MapData, Persistence

---
+++ MoveMate/Models/Destination.swift
@@ -0,0 +1,8 @@
+//
+//  Destination.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 30/4/2567 BE.
+//
+
+import Foundation
---
+++ MoveMate/Models/MTPlacemark.swift
@@ -0,0 +1,8 @@
+//
+//  MTPlacemark.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 30/4/2567 BE.
+//
+
+import Foundation
