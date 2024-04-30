333de04886e53525bed908c3ef9e7ef0dc0fb8c2
Shiberger <hannarong.tonkra@gmail.com>
2024-05-01 02:34:31 +0700

    add full func SelectView and Lookaround

--- MoveMate/Destination Tab/DestinationLocationsMapView.swift
+++ MoveMate/Destination Tab/DestinationLocationsMapView.swift
@@ -10,7 +10,6 @@ import MapKit
 import SwiftData
 
 struct DestinationLocationsMapView: View {
-    
     @Environment(\.modelContext) private var modelContext
     @State private var cameraPosition: MapCameraPosition = .automatic
     @State private var visibleRegion: MKCoordinateRegion?
@@ -22,6 +21,7 @@ struct DestinationLocationsMapView: View {
     }
     var destination: Destination
     
+    @State private var selectedPlacemark: MTPlacemark?
     var body: some View {
         @Bindable var destination = destination
         VStack {
@@ -49,22 +49,33 @@ struct DestinationLocationsMapView: View {
             }
         }
         .padding(.horizontal)
-        Map(position: $cameraPosition) {
+        Map(position: $cameraPosition, selection: $selectedPlacemark) {
             ForEach(listPlacemarks) { placemark in
-                if placemark.destination != nil {
-                    Marker(coordinate: placemark.coordinate) {
-                        Label(placemark.name, systemImage: "star")
+                Group {
+                    if placemark.destination != nil {
+                        Marker(coordinate: placemark.coordinate) {
+                            Label(placemark.name, systemImage: "star")
+                        }
+                        .tint(.orange)
+                    } else {
+                        Marker(placemark.name, coordinate: placemark.coordinate)
                     }
-                    .tint(.yellow)
-                } else {
-                    Marker(placemark.name, coordinate: placemark.coordinate)
-                }
+                }.tag(placemark)
             }
+        }
+        .sheet(item: $selectedPlacemark) { selectedPlacemark in
+            LocationDetailView(
+                destination: destination,
+                selectedPlacemark: selectedPlacemark
+            )
+                .presentationDetents([.height(450)])
         }
         .safeAreaInset(edge: .bottom) {
             HStack {
                 TextField("Search...", text: $searchText)
                     .textFieldStyle(.roundedBorder)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
                     .focused($searchFieldFocus)
                     .overlay(alignment: .trailing) {
                         if searchFieldFocus {
@@ -73,7 +84,7 @@ struct DestinationLocationsMapView: View {
                                 searchFieldFocus = false
                             } label: {
                                 Image(systemName: "xmark.circle.fill")
-                                    .tint(.appBlue)
+                                    .tint(.red)
                             }
                             .offset(x: -5)
                         }
@@ -86,6 +97,7 @@ struct DestinationLocationsMapView: View {
                                 visibleRegion: visibleRegion
                             )
                             searchText = ""
+                            cameraPosition = .automatic
                         }
                     }
                 if !searchPlacemarks.isEmpty {
