0f055ab7987523e5205eb2e5c7689d3776aa3cd5
Shiberger <hannarong.tonkra@gmail.com>
2024-05-02 18:27:33 +0700

    add func user location and location manager

---
+++ MoveMate/Services/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 2/5/2567 BE.
+//
+
+import Foundation
---
+++ MoveMate/Shared Views/LocationDeniedView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDeniedView.swift
+//  MoveMate
+//
+//  Created by Hannarong Kaewkiriya on 2/5/2567 BE.
+//
+
+import SwiftUI
+
+struct LocationDeniedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDeniedView()
+}
